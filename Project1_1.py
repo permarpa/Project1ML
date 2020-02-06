311e92f825725bea3926aefd92cd776c54b3c2ee
TheEllimist101 <60101128+TheEllimist101@users.noreply.github.com>
2020-02-06 11:27:15 +0100

    Add files via upload
    This is the modified version of the first code. I have added the Bootstrap framework (last two sections) and the code is supposed to plot the error, bias and variance of the training and test data under OLS, Ridge and Lasso regression, as a function of the regularisation paramaters lamda. The problem is that it's too hefty got my PC, so if someone has access to a more powerful system, please run the code on that and lemme know the output please :)

--- Project1_1.py
+++ Project1_1.py
@@ -2,22 +2,24 @@
 """
 Created on Sun Feb  2 20:24:47 2020
 
-@author: acer pc
+@authors: Bharat Mishra, Patricia Perez-Martin, Pinelopi Christodoulou
 """
 
 """Project 1 - Ising Model - Part (a)+(b)"""
 
+# %%
 """Import all necessary Python directories"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.linear_model as skl
 from sklearn.metrics import mean_squared_error as MSE
-from sklearn.metrics import r2_score as R2
+#from sklearn.metrics import r2_score as R2
 from sklearn.model_selection import train_test_split as splitter
 from sklearn.utils import resample
 import seaborn as sns
 
+# %%
 """Part (a) - Create dataset"""
 
 np.random.seed(12)      
@@ -47,6 +49,10 @@ def Ising_Energy(state):
     
 Energy=Ising_Energy(state)          #y values
 
+# %%
+
+"""Part (b) - Perform regression"""
+
 #Define function to create design matrix
 
 def Design_Matrix(state):              #Design matrix consists of all 2 spin interactions
@@ -59,12 +65,15 @@ def Design_Matrix(state):              #Design matrix consists of all 2 spin int
             for l in range(L):
                 X[i,j]=int(state[i,k]*state[i,l])
                 j+=1
+    Xmean=np.ones(n)
+    Xmean=np.reshape(Xmean,(n,1))
+    X=np.hstack((Xmean,X))
     return X
 
 #Define function to calculate the learned interaction
     
 def Learned_Interaction(coeff):
-    IntMat=np.reshape(coeff,(40,40))
+    IntMat=np.reshape(coeff[1:],(40,40))
     return IntMat
      
 #Construct the design matrix
@@ -73,11 +82,11 @@ X=Design_Matrix(state)
 
 #Split the data into training and test
 
-X_train,X_test,y_train,y_test=splitter(X,Energy,test_size=0.2)
+X_train,X_test,y_train,y_test=splitter(X,Energy,test_size=0.96)
 
-#Define vector of hyperparameter values for OLS< Ridge and Lasso and perfirm regression
+#Define vector of hyperparameter values for OLS, Ridge and Lasso and perform regression
 
-lamda=np.logspace(-8,-1,8)
+lamda=np.logspace(-4,4,9)
 
 MSE_OLS_train=np.zeros(len(lamda))
 MSE_Ridge_train=np.zeros(len(lamda))
@@ -93,7 +102,14 @@ R2_OLS_test=np.zeros(len(lamda))
 R2_Ridge_test=np.zeros(len(lamda))
 R2_Lasso_test=np.zeros(len(lamda))
 
+#Define function to calculate R2 values
 
+def R2(ypred,ytrue):
+    term1=np.sum((ytrue-ypred)**2)
+    term2=np.mean(ypred)
+    term3=np.sum((ytrue-term2)**2)
+    R2score=1-(term1/term3)
+    return R2score
 
 for i in range(len(lamda)):
     clf=skl.LinearRegression(fit_intercept=False).fit(X_train,y_train)
@@ -127,15 +143,17 @@ for i in range(len(lamda)):
     R2_Lasso_test[i]=R2(ypred_test,y_test)
     
 
-"""Plot data"""
+#%%%
+
+"""Plot unsampled results"""
 
 plt.figure()
-plt.semilogx(lamda,MSE_OLS_train,label='Train (OLS)',color='blue')
-plt.semilogx(lamda,MSE_OLS_test,label='Test (OLS)',color='blue',marker='+')
-plt.semilogx(lamda,MSE_Ridge_train,label='Train (Ridge)',color='red')
-plt.semilogx(lamda,MSE_Ridge_test,label='Test (Ridge)',color='red',marker='+')
-plt.semilogx(lamda,MSE_Lasso_train,label='Train (Lasso)',color='green')
-plt.semilogx(lamda,MSE_Lasso_test,label='Test (Lasso)',color='green',marker='+')
+plt.semilogx(lamda,MSE_OLS_train,'b',label='Train (OLS)')
+plt.semilogx(lamda,MSE_OLS_test,'--b',label='Test (OLS)')
+plt.semilogx(lamda,MSE_Ridge_train,'r',label='Train (Ridge)')
+plt.semilogx(lamda,MSE_Ridge_test,'--r',label='Test (Ridge)')
+plt.semilogx(lamda,MSE_Lasso_train,'g',label='Train (Lasso)')
+plt.semilogx(lamda,MSE_Lasso_test,'--g',label='Test (Lasso)')
 plt.legend()
 plt.xlabel('Hyperparameters')
 plt.ylabel('MSE')
@@ -143,12 +161,12 @@ plt.title('MSE')
 plt.show()
 
 plt.figure()
-plt.semilogx(lamda,R2_OLS_train,label='Train (OLS)',color='blue')
-plt.semilogx(lamda,R2_OLS_test,label='Test (OLS)',color='blue',marker='+')
-plt.semilogx(lamda,R2_Ridge_train,label='Train (Ridge)',color='red')
-plt.semilogx(lamda,R2_Ridge_test,label='Test (Ridge)',color='red',marker='+')
-plt.semilogx(lamda,R2_Lasso_train,label='Train (Lasso)',color='green')
-plt.semilogx(lamda,R2_Lasso_test,label='Test (Lasso)',color='green',marker='+')
+plt.semilogx(lamda,R2_OLS_train,'b',label='Train (OLS)')
+plt.semilogx(lamda,R2_OLS_test,'--b',label='Test (OLS)')
+plt.semilogx(lamda,R2_Ridge_train,'r',label='Train (Ridge)')
+plt.semilogx(lamda,R2_Ridge_test,'--r',label='Test (Ridge)')
+plt.semilogx(lamda,R2_Lasso_train,'g',label='Train (Lasso)')
+plt.semilogx(lamda,R2_Lasso_test,'--g',label='Test (Lasso)')
 plt.legend()
 plt.xlabel('Hyperparameters')
 plt.ylabel('R2')
@@ -156,18 +174,137 @@ plt.title('R2')
 plt.show()
 
 plt.figure()
-ax=sns.heatmap(OLS_IntMat)
+ax=sns.heatmap(OLS_IntMat,vmin=-1.0,vmax=1.0,cmap='seismic')
 plt.title('Lamda=1e-4, OLS')
 plt.show()
 
 plt.figure()
-ax=sns.heatmap(Ridge_IntMat)
+ax=sns.heatmap(Ridge_IntMat,vmin=-1.0,vmax=1.0,cmap='seismic')
 plt.title('Lamda=1e-4, Ridge')
 plt.show()
                     
 plt.figure()
-ax=sns.heatmap(Lasso_IntMat)
+ax=sns.heatmap(Lasso_IntMat,vmin=-1.0,vmax=1.0,cmap='seismic')
 plt.title('Lamda=1e-4, Lasso')
 plt.show()
 
+# %%
+
+"""Apply Bootstrap framework"""
+
+#Define Bootstrap parameters
+
+n_bootstrap=500
+MSE_boot_OLS_train=np.zeros(len(lamda))
+MSE_boot_OLS_test=np.zeros(len(lamda))
+MSE_boot_Ridge_train=np.zeros(len(lamda))
+MSE_boot_Ridge_test=np.zeros(len(lamda))
+MSE_boot_Lasso_train=np.zeros(len(lamda))
+MSE_boot_Lasso_test=np.zeros(len(lamda))
+Var_OLS_train=np.zeros(len(lamda))
+Var_OLS_test=np.zeros(len(lamda))
+Var_Ridge_train=np.zeros(len(lamda))
+Var_Ridge_test=np.zeros(len(lamda))
+Var_Lasso_train=np.zeros(len(lamda))
+Var_Lasso_test=np.zeros(len(lamda))
+Bias_OLS_train=np.zeros(len(lamda))
+Bias_OLS_test=np.zeros(len(lamda))
+Bias_Ridge_train=np.zeros(len(lamda))
+Bias_Ridge_test=np.zeros(len(lamda))
+Bias_Lasso_train=np.zeros(len(lamda))
+Bias_Lasso_test=np.zeros(len(lamda))
+
+y_train=np.reshape(y_train,(len(y_train),1))
+y_test=np.reshape(y_test,(len(y_test),1))
+ypred_train_OLS=np.zeros((len(y_train),n_bootstrap))
+ypred_test_OLS=np.zeros((len(y_test),n_bootstrap))
+ypred_train_Ridge=np.zeros((len(y_train),n_bootstrap))
+ypred_test_Ridge=np.zeros((len(y_test),n_bootstrap))
+ypred_train_Lasso=np.zeros((len(y_train),n_bootstrap))
+ypred_test_Lasso=np.zeros((len(y_test),n_bootstrap))
+
+for i in range(len(lamda)):
+    for j in range(n_bootstrap):
+        X_,y_=resample(X_train,y_train)
+        clf=skl.LinearRegression(fit_intercept=False).fit(X_,y_)
+        ypred_train_OLS[:,j]=clf.predict(X_).flatten()
+        ypred_test_OLS[:,j]=clf.predict(X_test).flatten()
+        
+        clf=skl.Ridge(alpha=lamda[i],fit_intercept=False).fit(X_,y_)
+        ypred_train_Ridge[:,j]=clf.predict(X_).flatten()
+        ypred_test_Ridge[:,j]=clf.predict(X_test).flatten()
+        
+        clf=skl.Lasso(alpha=lamda[i],fit_intercept=False).fit(X_,y_)
+        ypred_train_Lasso[:,j]=clf.predict(X_).flatten()
+        ypred_test_Lasso[:,j]=clf.predict(X_test).flatten()
+        
+    MSE_boot_OLS_train[i]=np.mean(np.mean((y_train-ypred_train_OLS)**2,axis=1,keepdims=True))
+    MSE_boot_OLS_test[i]=np.mean(np.mean((y_test-ypred_test_OLS)**2,axis=1,keepdims=True))
+    MSE_boot_Ridge_train[i]=np.mean(np.mean((y_train-ypred_train_Ridge)**2,axis=1,keepdims=True))
+    MSE_boot_Ridge_test[i]=np.mean(np.mean((y_test-ypred_test_Ridge)**2,axis=1,keepdims=True))
+    MSE_boot_Lasso_train[i]=np.mean(np.mean((y_train-ypred_train_Lasso)**2,axis=1,keepdims=True))
+    MSE_boot_Lasso_test[i]=np.mean(np.mean((y_test-ypred_test_Lasso)**2,axis=1,keepdims=True))
+    
+    Var_OLS_train[i]=np.mean(np.var(ypred_train_OLS,axis=1,keepdims=True))
+    Var_OLS_test[i]=np.mean(np.var(ypred_test_OLS,axis=1,keepdims=True))
+    Var_Ridge_train[i]=np.mean(np.var(ypred_train_Ridge,axis=1,keepdims=True))
+    Var_Ridge_test[i]=np.mean(np.var(ypred_test_Ridge,axis=1,keepdims=True))
+    Var_Lasso_train[i]=np.mean(np.var(ypred_train_Lasso,axis=1,keepdims=True))
+    Var_Lasso_test[i]=np.mean(np.var(ypred_test_Lasso,axis=1,keepdims=True))
+    
+    Bias_OLS_train[i]=np.mean((y_train-np.mean(ypred_train_OLS,axis=1,keepdims=True))**2)
+    Bias_OLS_test[i]=np.mean((y_test-np.mean(ypred_test_OLS,axis=1,keepdims=True))**2)
+    Bias_Ridge_train[i]=np.mean((y_train-np.mean(ypred_train_Ridge,axis=1,keepdims=True))**2)
+    Bias_Ridge_test[i]=np.mean((y_test-np.mean(ypred_test_Ridge,axis=1,keepdims=True))**2)
+    Bias_Lasso_train[i]=np.mean((y_train-np.mean(ypred_train_Lasso,axis=1,keepdims=True))**2)
+    Bias_Lasso_test[i]=np.mean((y_test-np.mean(ypred_test_Lasso,axis=1,keepdims=True))**2)
+
+#%%
+
+"""Plot sampled results"""
+
+plt.figure()
+plt.semilogx(lamda,MSE_boot_OLS_train,'b',label='Train Error')
+plt.semilogx(lamda,MSE_boot_OLS_test,'--b',label='Test Error')
+plt.semilogx(lamda,Var_OLS_train,'r',label='Train Variance')
+plt.semilogx(lamda,Var_OLS_test,'--r',label='Test Variance')
+plt.semilogx(lamda,Bias_OLS_train,'g',label='Train Bias')
+plt.semilogx(lamda,Bias_OLS_test,'--g',label='Test Bias')
+plt.legend()
+plt.xlabel('Hyperparameters')
+plt.ylabel('Errors')
+plt.title('Bias-Variance Analysis (OLS)')
+
+
+plt.figure()
+plt.semilogx(lamda,MSE_boot_Ridge_train,'b',label='Train Error')
+plt.semilogx(lamda,MSE_boot_Ridge_test,'--b',label='Test Error')
+plt.semilogx(lamda,Var_Ridge_train,'r',label='Train Variance')
+plt.semilogx(lamda,Var_Ridge_test,'--r',label='Test Variance')
+plt.semilogx(lamda,Bias_Ridge_train,'g',label='Train Bias')
+plt.semilogx(lamda,Bias_Ridge_test,'--g',label='Test Bias')
+plt.legend()
+plt.xlabel('Hyperparameters')
+plt.ylabel('Errors')
+plt.title('Bias-Variance Analysis (Ridge)')
+
+plt.figure()
+plt.semilogx(lamda,MSE_boot_Lasso_train,'b',label='Train Error')
+plt.semilogx(lamda,MSE_boot_Lasso_test,'--b',label='Test Error')
+plt.semilogx(lamda,Var_Lasso_train,'r',label='Train Variance')
+plt.semilogx(lamda,Var_Lasso_test,'--r',label='Test Variance')
+plt.semilogx(lamda,Bias_Lasso_train,'g',label='Train Bias')
+plt.semilogx(lamda,Bias_Lasso_test,'--g',label='Test Bias')
+plt.legend()
+plt.xlabel('Hyperparameters')
+plt.ylabel('Errors')
+plt.title('Bias-Variance Analysis (Lasso)')
+
+
+
+
+    
+    
+    
+    
 
