7633cb8b0cbc84ec668e63c55c6372d0820ab3eb
TheEllimist101 <60101128+TheEllimist101@users.noreply.github.com>
2020-02-15 23:49:26 +0100

    Add files via upload
    Modified and debugged version of Project 1 - part (d). We can play around with different parameters as part of the analysis.

--- Project1_3.py
+++ Project1_3.py
@@ -20,6 +20,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import seaborn as sns
 from sklearn.model_selection import train_test_split as splitter
+from sklearn.metrics import r2_score
 
 # %%
 
@@ -52,17 +53,20 @@ def Ising_Energy(state):
     
 Energy=Ising_Energy(state)          #y values
 
-x_train,x_test,y_train,y_test=splitter(state,Energy,test_size=0.96) #Split data into train and test (96% test)
+x_train,x_test,y_train,y_test=splitter(state,Energy,test_size=0.20) #Split data into train and test (96% test)
+y_train=np.reshape(y_train,(len(y_train),1))
+y_test=np.reshape(y_test,(len(y_test),1))
 
 # %%
 
 """Define tunable model parameters"""
 
-eta=np.logspace(-5,1,7)           #Define vector of learning rates (parameter to SGD optimiser)
-lamda=np.logspace(-4,4,9)         #Define vector of hyperparameters 
-n_layers=1                        #Define number of hidden layers in the model
-n_neuron=(x_train.shape[1])**2    #Define number of neurons per layer in the model (L^2 here for simplicity)
-epochs=100                        #Number of reiterations over the input data
+eta=np.logspace(-3,-1,3)           #Define vector of learning rates (parameter to SGD optimiser)
+lamda=np.logspace(-4,-2,3)         #Define vector of hyperparameters 
+n_layers=2                         #Define number of hidden layers in the model
+#n_neuron=(x_train.shape[1])       #Define number of neurons per layer in the model (L here for simplicity)
+n_neuron=128
+epochs=50                         #Number of reiterations over the input data
 batch_size=50                     #Number of samples per gradient update
 
 # %%
@@ -72,7 +76,7 @@ batch_size=50                     #Number of samples per gradient update
 def R2_score(y_true,y_pred):
     SS_res=K.sum(K.square(y_true-y_pred)) 
     SS_tot=K.sum(K.square(y_true-K.mean(y_pred))) 
-    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+    return ( 1 - SS_res/(SS_tot) )
 
 # %%
 
@@ -81,38 +85,42 @@ def R2_score(y_true,y_pred):
 def NN_Model(n_layers,n_neuron,eta,lamda):
     model=Sequential()
     for i in range(n_layers):              #Run loop to add hidden layers to model
-        model.add(Dense(n_neuron,activation='sigmoid',kernel_regularizer=regularizers.l1(lamda))) #Using l1
+        if (i==0):                         #First layer requires input dimensions = 40 spins
+            model.add(Dense(n_neuron,activation='relu',kernel_regularizer=regularizers.l2(lamda),input_dim=40))
+        else:                              #Corresponding layers are capable of automatic shape inference
+            model.add(Dense(n_neuron,activation='relu',kernel_regularizer=regularizers.l2(lamda))) #Using l2
     model.add(Dense(1,activation='relu'))      #add output layer to model
-    
     sgd=optimizers.SGD(lr=eta)    #Define optimiser for model (lr is learning rate)
-    model.compile(loss='mean_squared_error',optimizer=sgd,metrics=[R2_score])
+    model.compile(loss='mean_squared_error',optimizer=sgd)   #can add metric here, but not necessary
     return model
     
 # %%
     
-"""Perform regression on data)"""
+"""Perform regression on data"""
 
-R2_NN=np.zeros((len(lamda),len(eta)),dtype=object)    #Define 2D matrix to store R2 scores (variation with hyperparameters
-                                                      #and learning rates)
-R2_train=np.zeros((len(lamda),len(eta)))
-R2_test=np.zeros((len(lamda),len(eta)))
+R2_train=np.zeros((len(lamda),len(eta)))     #Define vector to store R2 metric for training data
+R2_test=np.zeros((len(lamda),len(eta)))      #Define vector to store R2 metric for testing data
 
-for i in range(len(lamda)):
+for i in range(len(lamda)):                  #Run loops over hyperparamaters and learning rates
     for j in range(len(eta)):
-        DNN_model=NN_Model(n_layers,n_neuron,eta[j],lamda[i])
-        DNN_model.fit(x_train,y_train,epochs=epochs,batch_size=batch_size,verbose=0)
-        R2_train[i,j]=DNN_model.evaluate(x_train,y_train)[1]
-        R2_test[i,j]=DNN_model.evaluate(x_test,y_test)[1]
+        DNN_model=NN_Model(n_layers,n_neuron,eta[j],lamda[i])   #Call model for each lamda and eta
+        DNN_model.fit(x_train,y_train,epochs=epochs,batch_size=batch_size,verbose=1)  #training on data
+        y_train_pred=DNN_model.predict(x_train)       #predict y_train values
+        y_test_pred=DNN_model.predict(x_test)         #predict y_test values
+        R2_train[i,j]=r2_score(y_train,y_train_pred)  #calculate R2 scores 
+        R2_test[i,j]=r2_score(y_test,y_test_pred)
         
 # %% 
         
 """Plot results (no Bootstrap framework)"""
 
 plt.figure()
-plt.semilogx(lamda,R2_train[:,0],'--g',label='Learning Rate=1E-5, Train Data')
-plt.semilogx(lamda,R2_test[:,0],'-*g',label='Learning Rate=1E-5, Test Data')
-plt.semilogx(lamda,R2_train[:,5],'--r',label='Learning Rate=1E+0, Train Data')
-plt.semilogx(lamda,R2_test[:,5],'-*r',label='Learning Rate=1E+0, Test Data')
+plt.semilogx(lamda,R2_train[:,0],'--g',label='Learning Rate=1E-3, Train Data')
+plt.semilogx(lamda,R2_test[:,0],'-*g',label='Learning Rate=1E-3, Test Data')
+plt.semilogx(lamda,R2_train[:,1],'--r',label='Learning Rate=1E-2, Train Data')
+plt.semilogx(lamda,R2_test[:,1],'-*r',label='Learning Rate=1E-2, Test Data')
+plt.semilogx(lamda,R2_train[:,2],'--b',label='Learning Rate=1E-1, Train Data')
+plt.semilogx(lamda,R2_test[:,2],'-*b',label='Learning Rate=1E-1, Test Data')
 plt.legend()
 plt.xlabel('Hyperparameters')
 plt.ylabel('R2')
